8f75edb39cc32261c0e693b628bc3418b26ab53e
ArnavJain0902 <arnavjain0902@gmail.com>
2024-12-19 20:27:40 +0530

    savable names

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,27 +1,38 @@
 
 import {useState} from 'react'
 
-export default function Player({name,symbol,children}){
+export default function Player({initialName,symbol}){
     const [isEditing, setIsEditing] = useState(false)
+    const [playerName,setPlayerName]= useState(initialName);
+
+    
     function handleEditClick(){
-        setIsEditing(true)
+        setIsEditing((editing)=>!editing)
     }
-    let playerName;
-    let save='Edit'
+
+    function handleChange(event){
+        setPlayerName(event.target.value)
+    }
+
+
+    let editablePlayerName;
+    let btnCaption='Edit'
     if(isEditing===false){
-        playerName = <span className="player-name">{name}</span>
+        editablePlayerName= <span className="player-name">{playerName}</span>
     }else{
-        playerName = <input type='text' required></input>
-        save = 'Save'
+        editablePlayerName = <input type='text' value={playerName} required onChange={handleChange}></input>
+        btnCaption = 'Save'
     }
+
+
     return(
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
                 
             </span>
-            <button onClick={()=>{handleEditClick()}}>{save}</button>
+            <button onClick={()=>{handleEditClick()}}>{btnCaption}</button>
               
         </li>
     )
